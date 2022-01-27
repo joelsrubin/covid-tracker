21db865916494e8247d93c1d4e81fdb524ebbbe8
joelsrubin <joelsrubin@gmail.com>
2022-01-27 11:44:39 -0600

    add days til

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -2,6 +2,13 @@ import covid from './covid.png';
 import { useQuery } from 'react-query';
 
 const Landing: React.FC = () => {
+  const spring = new Date('03/21/2022');
+  const today = new Date();
+  // get the difference in days
+  const diff = Math.floor(
+    (spring.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
+  );
+
   const { data } = useQuery('landing', () =>
     fetch('/.netlify/functions/data').then((res) => res.json())
   );
@@ -19,6 +26,7 @@ const Landing: React.FC = () => {
         Projected Total:{' '}
         {Math.floor(projectedTotal).toLocaleString('en-US') || 0}{' '}
       </h2>
+      <h2>Days Until Spring: {diff || 0} </h2>
       <img src={covid} className='App-logo' />
     </header>
   );
